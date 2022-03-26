7f75a86aef00e5bf0655ab8aff0407aa9e306d3b
suiyebuluo <guoju58@163.com>
2022-03-26 16:53:14 +0800

    update jdbc sqlserver test

--- src/main/scala/com/dt/spark/cores/JDBCTest.scala
+++ src/main/scala/com/dt/spark/cores/JDBCTest.scala
@@ -5,7 +5,8 @@ import org.apache.spark.sql.SparkSession
 object JDBCTest {
 //  val
   def main(args: Array[String]): Unit = {
-    val dbServer = "172.16.244.158"
+    // IP host
+    val dbServer = ""
 
     val db = "TestDB"
 
@@ -16,15 +17,13 @@ object JDBCTest {
       .master("local[*]")
       .appName("Test")
       .getOrCreate()
-    val query = "SELECT * FROM Inventory"
+    val query = "SELECT * FROM Test123.dbo.Inventory"
     val jdbcDF = spark.read
       .format("jdbc")
       .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
       .option("url", url)
       .option("user", "SA")
       .option("password", "MyPassWord123")
-//      .option("dbtable", "Inventory")
-//      .option("dbtable", "SELECT * FROM Inventory GO")
       .option("dbtable", s"($query) as my_table")
       .load()
 
